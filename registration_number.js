00caeaa5801a9c6fdc4563b7d426f06418163a1f
Lakhiwe <siyasangalakhiwe6@gmail.com>
2017-03-20 16:40:16 +0200

    filtering the DOM elemets to select different towns

--- registration_number.js
+++ registration_number.js
@@ -1,5 +1,34 @@
 var addButton = document.getElementById('regAdd');
 var input = addButton.querySelector('input');
+var opt = document.getElementById('options')
+
+// function regNum(registrations) {
+//   return registrations.startsWith('EC');
+//       || registrations.startsWith('CA');
+//       || registrations.startsWith('GP');
+//       || registrations.startsWith('ND');
+// }
+
+
+opt.addEventListener('change', function (evt) {
+  var chosenRegs = evt.target.value;
+  var regs = document.querySelectorAll('li');
+
+  for (var i = 0; i < regs.length; i++) {
+      var elem = regs[i];
+
+    if (evt.target.value === 'Show All') {
+        elem.style.display = 'block';
+    }
+    else if (elem.innerHTML.startsWith(evt.target.value)) {
+      elem.style.display = 'block';
+    }
+    else {
+        elem.style.display = 'none';
+    }
+  }
+
+});
 
 addButton.addEventListener('submit', (e) => {
 
